58ed840561975067f08dcbe158135b0db4f673cd
Haziq Musa <last.game01987@gmail.com>
2022-12-30 06:35:42 +0800

    new component

---
+++ src/main/java/com/callback/employeeis/components/Event.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.callback.employeeis.components;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author HAZIQ
+ */
+public class Event {
+
+  public Event() {
+  }
+  
+  public static boolean onEnter(KeyEvent evt) {
+    return evt.getKeyCode() == 10;
+  }
+  
+}
